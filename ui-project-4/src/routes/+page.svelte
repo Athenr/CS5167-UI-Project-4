2ed7018d9cadd64fd5bd154cfdea559145aacf3a
Athenr <82354163+Athenr@users.noreply.github.com>
2025-12-09 23:04:58 -0500

    update2

--- ui-project-4/src/routes/+page.svelte
+++ ui-project-4/src/routes/+page.svelte
@@ -1,2 +1,310 @@
-<h1>Welcome to asdadsdSvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import news1 from "$lib/assets/news1.png";
+    import news2 from "$lib/assets/news2.png";
+    import news3 from "$lib/assets/news3.png";
+</script>
+
+<div id="homePage">
+    <div id="info">
+        <div id="alerts">
+            <div class="header">
+                Equine Alerts
+            </div>
+
+            <div class="message alertMessage blue">
+                <div class="iconBox">
+                    <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M28.2222 1L36 8.77778M32.1111 4.88889L23.3611 13.6389M17.5278 7.80556L29.1944 19.4722M27.25 17.5278L14.6111 30.1667H6.83333M6.83333 30.1667V22.3889L19.4722 9.75M6.83333 30.1667L1 36M9.75 19.4722L12.6667 22.3889M15.5833 13.6389L18.5 16.5556" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <span class="messageText"><span class="messageTextBold">Bucephalus</span> is due for an annual Equine Influenza vaccine on 12/3.</span>
+            </div>
+            <div class="message alertMessage red">
+                <div class="iconBox">
+                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.5 35C17.0345 35 16.5905 34.9127 16.1681 34.7382C15.7456 34.5636 15.3604 34.3163 15.0125 33.9963L1.00374 19.9875C0.683707 19.6384 0.436409 19.2526 0.261845 18.8302C0.0872817 18.4077 0 17.9643 0 17.5C0 17.0357 0.0872817 16.5847 0.261845 16.1471C0.436409 15.7096 0.683707 15.3313 1.00374 15.0125L15.0125 1.00375C15.3616 0.654626 15.7474 0.399763 16.1698 0.239165C16.5923 0.0785661 17.0357 -0.0011512 17.5 1.25585e-05C17.9643 0.00117632 18.4153 0.0814754 18.8529 0.24091C19.2904 0.400345 19.6687 0.654626 19.9875 1.00375L33.9962 15.0125C34.3454 15.3325 34.6002 15.7107 34.7608 16.1471C34.9214 16.5835 35.0011 17.0345 35 17.5C34.9988 17.9655 34.9191 18.4095 34.7608 18.8319C34.6026 19.2544 34.3477 19.6396 33.9962 19.9875L19.9875 33.9963C19.6675 34.3163 19.2893 34.5636 18.8529 34.7382C18.4165 34.9127 17.9655 35 17.5 35ZM15.7544 19.2456H19.2456V8.77183H15.7544V19.2456ZM17.5 24.4825C17.9946 24.4825 18.4095 24.315 18.7446 23.9798C19.0798 23.6446 19.2468 23.2303 19.2456 22.7369C19.2445 22.2435 19.0769 21.8292 18.7429 21.494C18.4089 21.1589 17.9946 20.9913 17.5 20.9913C17.0054 20.9913 16.5911 21.1589 16.2571 21.494C15.9231 21.8292 15.7555 22.2435 15.7544 22.7369C15.7532 23.2303 15.9208 23.6452 16.2571 23.9816C16.5934 24.3179 17.0077 24.4849 17.5 24.4825Z" fill="#C00000"/>
+                    </svg>
+                </div>
+                <span class="messageText">Records for <span class="messageTextBold">Epona</span> shows a high risk of colic!</span>
+            </div>
+            <div class="message alertMessage blue">
+                <div class="iconBox">
+                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M29.75 4.77273H31.5V0H24.5V4.77273C24.5 4.77273 28 11.1364 28 17.5C28 23.8636 24.5 28.6364 17.5 28.6364C10.5 28.6364 7 23.8636 7 17.5C7 11.1364 10.5 4.77273 10.5 4.77273V0H3.5V4.77273H5.25C5.25 4.77273 0 11.1364 0 20.6818C0 28.6364 8.75 35 17.5 35C26.25 35 35 28.6364 35 20.6818C35 11.1364 29.75 4.77273 29.75 4.77273ZM3.5 19.0909C2.45 19.0909 1.75 18.4545 1.75 17.5C1.75 16.5455 2.45 15.9091 3.5 15.9091C4.55 15.9091 5.25 16.5455 5.25 17.5C5.25 18.4545 4.55 19.0909 3.5 19.0909ZM7 28.6364C5.95 28.6364 5.25 28 5.25 27.0455C5.25 26.0909 5.95 25.4545 7 25.4545C8.05 25.4545 8.75 26.0909 8.75 27.0455C8.75 28 8.05 28.6364 7 28.6364ZM17.5 33.4091C16.45 33.4091 15.75 32.7727 15.75 31.8182C15.75 30.8636 16.45 30.2273 17.5 30.2273C18.55 30.2273 19.25 30.8636 19.25 31.8182C19.25 32.7727 18.55 33.4091 17.5 33.4091ZM28 28.6364C26.95 28.6364 26.25 28 26.25 27.0455C26.25 26.0909 26.95 25.4545 28 25.4545C29.05 25.4545 29.75 26.0909 29.75 27.0455C29.75 28 29.05 28.6364 28 28.6364ZM31.5 19.0909C30.45 19.0909 29.75 18.4545 29.75 17.5C29.75 16.5455 30.45 15.9091 31.5 15.9091C32.55 15.9091 33.25 16.5455 33.25 17.5C33.25 18.4545 32.55 19.0909 31.5 19.0909Z" fill="black"/>
+                    </svg>
+                </div>
+                <span class="messageText"><span class="messageTextBold">Adagio</span> is due for a farrier appointment on <span class="messageTextBold">12/6</span>.</span>
+            </div>
+            <div class="message alertMessage green">
+                <div class="iconBox">
+                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.25 16.8V10.5C19.25 10.0042 19.082 9.58883 18.746 9.254C18.41 8.91916 17.9947 8.75116 17.5 8.75C17.0053 8.74883 16.59 8.91683 16.254 9.254C15.918 9.59116 15.75 10.0065 15.75 10.5V17.4562C15.75 17.6896 15.7937 17.9159 15.8812 18.1352C15.9687 18.3546 16.1 18.5512 16.275 18.725L22.05 24.5C22.3708 24.8208 22.7792 24.9812 23.275 24.9812C23.7708 24.9812 24.1792 24.8208 24.5 24.5C24.8208 24.1792 24.9812 23.7708 24.9812 23.275C24.9812 22.7792 24.8208 22.3708 24.5 22.05L19.25 16.8ZM17.5 35C15.0792 35 12.8042 34.5403 10.675 33.621C8.54583 32.7017 6.69375 31.4551 5.11875 29.8812C3.54375 28.3074 2.29717 26.4553 1.379 24.325C0.460835 22.1947 0.00116888 19.9197 2.21519e-06 17.5C-0.00116445 15.0803 0.458502 12.8053 1.379 10.675C2.2995 8.54467 3.54608 6.69258 5.11875 5.11875C6.69141 3.54492 8.5435 2.29833 10.675 1.379C12.8065 0.459667 15.0815 0 17.5 0C19.9185 0 22.1935 0.459667 24.325 1.379C26.4565 2.29833 28.3086 3.54492 29.8812 5.11875C31.4539 6.69258 32.7011 8.54467 33.6227 10.675C34.5444 12.8053 35.0035 15.0803 35 17.5C34.9965 19.9197 34.5368 22.1947 33.621 24.325C32.7051 26.4553 31.4586 28.3074 29.8812 29.8812C28.3039 31.4551 26.4518 32.7022 24.325 33.6227C22.1982 34.5432 19.9232 35.0023 17.5 35ZM17.5 31.5C21.3792 31.5 24.6826 30.1367 27.4102 27.4102C30.1379 24.6837 31.5011 21.3803 31.5 17.5C31.4988 13.6197 30.1356 10.3162 27.4102 7.58975C24.6849 4.86325 21.3815 3.5 17.5 3.5C13.6185 3.5 10.3151 4.86383 7.58975 7.5915C4.86442 10.3192 3.50117 13.622 3.5 17.5C3.49883 21.378 4.86267 24.6814 7.5915 27.4102C10.3203 30.1391 13.6232 31.5023 17.5 31.5Z" fill="black"/>
+                    </svg>
+                </div>
+                <span class="messageText">Turnout schedule for <span class="messageTextBold">Bucephalus</span> needs to be reviewed.</span>
+            </div>
+
+        </div>
+        <div id="status">
+            <div class="header">
+                Stable Status
+            </div>
+            <div class="message statusMessage blue">
+                <div class="iconBox">
+                    <svg width="35" height="30" viewBox="0 0 35 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M24.0532 0L0.00856824 3.89247L0 6.05339L24.4661 2.08606L24.0532 0ZM16.1004 4.73283L13.7636 5.11417V16.4413H16.1004V4.73283ZM3.63759 6.75251L1.30081 7.13384V16.4413H3.63759V6.75251ZM22.4487 8.74394C21.7788 9.17471 21.9034 10.4458 22.7213 11.5757C21.0388 12.1054 19.1694 12.7904 17.5025 13.3836V16.4413H18.7488V22.8605C21.9657 21.3916 24.544 19.1389 27.0677 16.5755C27.426 18.1856 29.7082 18.4045 31.048 18.6022C31.048 18.6022 31.1181 19.5415 31.5933 19.9157C32.3099 20.4666 33.4705 20.862 34.2883 20.4383C35.0439 20.0428 35.0049 18.6234 34.9972 18.3692C34.9816 17.8396 34.4285 17.4441 33.9145 17.2393C32.5124 14.7677 29.4434 10.8342 26.8418 10.6648C26.4835 10.6436 25.9694 10.7142 25.3618 10.8413C24.2947 10.1916 22.8615 9.16765 22.4487 8.74394ZM12.3616 14.8524C9.27701 15.0219 6.94802 15.036 5.03965 15.0643V16.4413H12.3616V14.8524ZM0.0545249 17.7124V20.9609H17.3467V17.7124H0.0545249ZM0.0545249 22.232V25.4804H17.3467V22.232H0.0545249ZM0.0545249 26.7516V30H17.3467V26.7516H0.0545249Z" fill="black"/>
+                    </svg>
+                </div>
+                <span class="messageText">Five horses are currently stabled.</span>
+            </div>
+            <div class="message statusMessage green">
+                <div class="iconBox">
+                    <svg width="35" height="26" viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M33.6538 17.9534H32.3077V9.90682H33.6538C34.0109 9.90682 34.3533 9.76553 34.6057 9.51402C34.8582 9.26252 35 8.9214 35 8.56572C35 8.21004 34.8582 7.86893 34.6057 7.61742C34.3533 7.36592 34.0109 7.22463 33.6538 7.22463H32.3077V1.86023L31.5377 0.323334C31.4786 0.226024 31.3956 0.145295 31.2965 0.088744C31.1974 0.0321925 31.0856 0.00167661 30.9714 6.71424e-05C30.8572 -0.00154233 30.7445 0.0258076 30.6439 0.0795437C30.5433 0.13328 30.458 0.211636 30.3962 0.307241L29.6154 1.86023V7.22463H26.9231V1.86023L26.1531 0.323334C26.094 0.226024 26.011 0.145295 25.9119 0.088744C25.8128 0.0321925 25.701 0.00167661 25.5868 6.71424e-05C25.4726 -0.00154233 25.3599 0.0258076 25.2593 0.0795437C25.1587 0.13328 25.0734 0.211636 25.0115 0.307241L24.2308 1.86023V7.22463H21.5385V1.86023L20.7685 0.323334C20.7093 0.226024 20.6263 0.145295 20.5273 0.088744C20.4282 0.0321925 20.3163 0.00167661 20.2022 6.71424e-05C20.088 -0.00154233 19.9753 0.0258076 19.8747 0.0795437C19.774 0.13328 19.6888 0.211636 19.6269 0.307241L18.8462 1.86023V7.22463H16.1538V1.86023L15.3838 0.323334C15.3247 0.226024 15.2417 0.145295 15.1427 0.088744C15.0436 0.0321925 14.9317 0.00167661 14.8176 6.71424e-05C14.7034 -0.00154233 14.5907 0.0258076 14.4901 0.0795437C14.3894 0.13328 14.3042 0.211636 14.2423 0.307241L13.4615 1.86023V7.22463H10.7692V1.86023L9.99923 0.323334C9.94011 0.226024 9.85711 0.145295 9.75805 0.088744C9.65898 0.0321925 9.5471 0.00167661 9.43294 6.71424e-05C9.31877 -0.00154233 9.20607 0.0258076 9.10544 0.0795437C9.00481 0.13328 8.91955 0.211636 8.85769 0.307241L8.07692 1.86023V7.22463H5.38462V1.86023L4.61462 0.323334C4.55549 0.226024 4.4725 0.145295 4.37343 0.088744C4.27437 0.0321925 4.16249 0.00167661 4.04832 6.71424e-05C3.93416 -0.00154233 3.82146 0.0258076 3.72083 0.0795437C3.6202 0.13328 3.53494 0.211636 3.47308 0.307241L2.69231 1.86023V7.22463H1.34615C0.989132 7.22463 0.646732 7.36592 0.394279 7.61742C0.141826 7.86893 0 8.21004 0 8.56572C0 8.9214 0.141826 9.26252 0.394279 9.51402C0.646732 9.76553 0.989132 9.90682 1.34615 9.90682H2.69231V17.9534H1.34615C0.989132 17.9534 0.646732 18.0947 0.394279 18.3462C0.141826 18.5977 0 18.9388 0 19.2945C0 19.6502 0.141826 19.9913 0.394279 20.2428C0.646732 20.4943 0.989132 20.6356 1.34615 20.6356H2.69231V24.6589C2.69231 25.0146 2.83413 25.3557 3.08659 25.6072C3.33904 25.8587 3.68144 26 4.03846 26C4.39548 26 4.73788 25.8587 4.99034 25.6072C5.24279 25.3557 5.38462 25.0146 5.38462 24.6589V20.6356H8.07692V24.6589C8.07692 25.0146 8.21875 25.3557 8.4712 25.6072C8.72366 25.8587 9.06605 26 9.42308 26C9.7801 26 10.1225 25.8587 10.375 25.6072C10.6274 25.3557 10.7692 25.0146 10.7692 24.6589V20.6356H13.4615V24.6589C13.4615 25.0146 13.6034 25.3557 13.8558 25.6072C14.1083 25.8587 14.4507 26 14.8077 26C15.1647 26 15.5071 25.8587 15.7596 25.6072C16.012 25.3557 16.1538 25.0146 16.1538 24.6589V20.6356H18.8462V24.6589C18.8462 25.0146 18.988 25.3557 19.2404 25.6072C19.4929 25.8587 19.8353 26 20.1923 26C20.5493 26 20.8917 25.8587 21.1442 25.6072C21.3966 25.3557 21.5385 25.0146 21.5385 24.6589V20.6356H24.2308V24.6589C24.2308 25.0146 24.3726 25.3557 24.625 25.6072C24.8775 25.8587 25.2199 26 25.5769 26C25.9339 26 26.2763 25.8587 26.5288 25.6072C26.7813 25.3557 26.9231 25.0146 26.9231 24.6589V20.6356H29.6154V24.6589C29.6154 25.0146 29.7572 25.3557 30.0097 25.6072C30.2621 25.8587 30.6045 26 30.9615 26C31.3186 26 31.661 25.8587 31.9134 25.6072C32.1659 25.3557 32.3077 25.0146 32.3077 24.6589V20.6356H33.6538C34.0109 20.6356 34.3533 20.4943 34.6057 20.2428C34.8582 19.9913 35 19.6502 35 19.2945C35 18.9388 34.8582 18.5977 34.6057 18.3462C34.3533 18.0947 34.0109 17.9534 33.6538 17.9534ZM5.38462 17.9534V9.90682H8.07692V17.9534H5.38462ZM10.7692 17.9534V9.90682H13.4615V17.9534H10.7692ZM16.1538 17.9534V9.90682H18.8462V17.9534H16.1538ZM21.5385 17.9534V9.90682H24.2308V17.9534H21.5385ZM26.9231 17.9534V9.90682H29.6154V17.9534H26.9231Z" fill="black"/>
+                    </svg>
+                </div>
+                <span class="messageText">Five horses are in turnout.</span>
+            </div>
+            <div class="message statusMessage red">
+                <div class="iconBox">
+                    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M35 23.3333H23.3333V35H11.6667V23.3333H0V11.6667H11.6667V0H23.3333V11.6667H35" fill="black"/>
+                    </svg>
+                </div>
+                <span class="messageText">Three horses are at an equine hospital.</span>
+            </div>
+        </div>
+    </div>
+    
+    <div id="newsFrame">
+        <div class="newsTemplate">
+            <div class="newsImageDiv">
+                <img src={news1} alt="News"/>
+            </div>
+            <div class="newsInfo">
+                <p class="newsDate">December 8, 2025</p>
+                <p class="newsText">Dates and Locations Announced for 2026 USEF Combined Driving National Championships</p>
+                <p class="newsSource">US Equestrian Communications</p>
+            </div>
+        </div>
+
+        <div class="newsTemplate">
+            <div class="newsImageDiv">
+                <img src={news2} alt="News"/>
+            </div>
+            <div class="newsInfo">
+                <p class="newsDate">December 1, 2025</p>
+                <p class="newsText">Valegro And Uthopia, British Dressage Stars, Have Died</p>
+                <p class="newsSource">The Chronicle of the Horse</p>
+            </div>
+        </div>
+        
+        <div class="newsTemplate">
+            <div class="newsImageDiv">
+                <img src={news3} alt="News"/>
+            </div>
+            <div class="newsInfo">
+                <p class="newsDate">November 21, 2025</p>
+                <p class="newsText">EHV-1 Update And Biosecurity Reminders From US Equestrian; As of November 21, The EHV-1 Outbreak Does Not Involve USEF-Licensed Events</p>
+                <p class="newsSource">US Equestrian Communications</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+#info {
+    display: flex;
+    padding: 10px 0;
+    align-items: flex-start;
+    gap: 20px;
+    flex: 1 0 0;
+    align-self: stretch;
+}
+
+#alerts {
+    display: flex;
+    width: 700px;
+    padding: 15px;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+    align-self: stretch;
+    border-radius: 20px;
+    background: rgba(202, 202, 202, 0.30);
+}
+
+#status {
+    display: flex;
+    width: 1120px;
+    padding: 15px;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+    align-self: stretch;
+    border-radius: 20px;
+    background: rgba(202, 202, 202, 0.30);
+}
+
+.header {
+    display: flex;
+    width: 650px;
+    padding-bottom: 8px;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 3px solid #FFF;
+}
+
+.iconBox {
+    display: flex;
+    width: 60px;
+    height: 61px;
+    padding: 14px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    aspect-ratio: 60/61;
+    border-radius: 50px;
+    background: #AAA;
+}
+.messageTextBold {
+    font-weight: 700;
+}
+
+.messageText {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.message {
+    display: flex;
+    height: 90px;
+    padding: 10px 18px;
+    align-items: center;
+    gap: 10px;
+    border-radius: 25px;
+}
+
+.statusMessage {
+    width: 1070px;
+}
+
+.alertMessage {
+    width: 650px;
+}
+
+.red {
+    background: linear-gradient(91deg, #4B2828 0%, #6E3D3D 100%);
+}
+
+.green {
+    background: linear-gradient(91deg, #284B28 0%, #3F6E3D 100%);
+}
+
+.blue {
+    background: linear-gradient(91deg, #28384B 0%, #3D546E 100%);
+}
+
+#homePage{
+    display: flex;
+    width: 1920px;
+    height: 1200px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+#newsFrame {
+    display: flex;
+    height: 350px;
+    padding: 10px 0;
+    align-items: flex-start;
+    gap: 20px;
+    flex-shrink: 0;
+    align-self: stretch;
+    }
+
+.newsTemplate {
+    display: flex;
+    width: 600px;
+    padding: 10px;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+    border-radius: 20px;
+    background: #CACACA;
+}
+
+.newsImageDiv {
+    display: flex;
+    width: 260px;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    flex-shrink: 0;
+}
+
+img {
+    width: 230px;
+    height: 310px;
+    flex-shrink: 0;
+    aspect-ratio: 23/31;
+    border-radius: 15px;
+}
+
+.newsInfo {
+    display: flex;
+    width: 310px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    flex-shrink: 0;
+    align-self: stretch;
+}
+
+.newsDate {
+    display: flex;
+    height: 31px;
+    flex-direction: column;
+    justify-content: center;
+    align-self: stretch;
+    color: #000;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: normal;
+}
+
+.newsText {
+    flex: 1 0 0;
+    align-self: stretch;
+    color: #000;
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+}
+
+.newsSource {
+    display: flex;
+    height: 31px;
+    flex-direction: column;
+    justify-content: center;
+    align-self: stretch;
+    color: #000;
+    font-family: Inter;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: normal;
+}
+</style>
---
+++ ui-project-4/src/routes/horses/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div id="horsePage">
+    <div id="barnBar">
+
+    </div>
+    <div id="horseInfo">
+        <div class="barnCategory">
+        </div>
+        <div class="barnCategory">
+        </div>
+    </div>
+</div>
+
+<style>
+
+</style>
